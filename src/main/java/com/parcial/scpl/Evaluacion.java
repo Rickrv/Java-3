0f6dcf8cb7f6cbbb4b4ca3c09c50786fc36f341f
T107 <T107@PC245>
2014-10-31 21:04:54 -0600

    Examen

---
+++ src/main/java/com/parcial/scpl/Evaluacion.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.parcial.scpl;
+
+/**
+ *
+ * @author T107
+ */
+public class Evaluacion {
+    private String nombre;
+    private float calificacion;
+
+    public Evaluacion() {
+    }
+
+    public Evaluacion(String nombre, float calificacion) {
+        this.nombre = nombre;
+        this.calificacion = calificacion;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the calificacion
+     */
+    public float getCalificacion() {
+        return calificacion;
+    }
+
+    /**
+     * @param calificacion the calificacion to set
+     */
+    public void setCalificacion(float calificacion) {
+        this.calificacion = calificacion;
+    }
+    
+}
---
+++ src/main/webapp/WEB-INF/presentacion.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : presentacion
+    Created on : 31-oct-2014, 20:20:01
+    Author     : T107
+--%>
+
+<%@page contentType="text/html" pageEncoding="windows-1252"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Resultado de calificaciones</h1>
+        <a id="resultados" class="Evaluacion" a></a>
+        <div "resultado exitoso" </div>
+    </body>
+</html>
